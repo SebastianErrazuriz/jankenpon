0e70f222562d77429ec317863b03c22766f83659
Thomas Maureira <thmaureira@gmail.com>
2022-04-14 16:25:25 -0400

    feat(games): add model game

--- games/models.py
+++ games/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Game(models.Model):
+    ROCK = 'ROCK'
+    PAPER = 'PAPER'
+    SCISSORS = 'SCISSORS'
+
+    HAND_CHOICES = [
+        (ROCK, 'rock'),
+        (PAPER, 'paper'),
+        (SCISSORS, 'scissors'),
+    ]
+    player = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    hand = models.CharField(
+        max_length=8,
+        choices=HAND_CHOICES,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
