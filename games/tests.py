cd3cae606a452d53f83205fee5060d3d75ddb66a
Thomas Maureira <thmaureira@gmail.com>
2022-04-14 16:43:56 -0400

    tests(games): add initial tests

--- games/tests.py
+++ games/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Game
+from django.contrib.auth.models import User
+
+
+class GameModelTests(TestCase):
+
+    def test_system_hand_paper_win_player_rock(self):
+        player = User.objects.create()
+        game = Game(player=player, hand=Game.ROCK, system_hand=Game.PAPER)
+        self.assertIs(game.does_system_hand_win(), True)
+
+    def test_system_hand_rock_win_player_scissors(self):
+        player = User.objects.create()
+        game = Game(player=player, hand=Game.SCISSORS, system_hand=Game.ROCK)
+        self.assertIs(game.does_system_hand_win(), True)
